a65ab60e286b7d5bcc3918c241d5e0634a7d25a1
leksu70 <82502776+leksu70@users.noreply.github.com>
2022-09-06 21:45:59 +0300

    Delete/Add

---
+++ src/main/java/s22/Bookstore/domain/Book.java
@@ -0,0 +1,93 @@
+package s22.Bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title, author;
+	private int bookYear;
+	private String isbn;
+	private double price;
+	
+	public Book() {
+		super();
+	}
+
+	public Book(String title, String author, int bookYear, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.bookYear = bookYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public Book(String title, String author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getbookYear() {
+		return bookYear;
+	}
+
+	public void setbookYear(int bookYear) {
+		this.bookYear = bookYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", bookYear=" + bookYear + ", isbn="
+				+ isbn + ", price=" + price + "]";
+	}
+		
+}
