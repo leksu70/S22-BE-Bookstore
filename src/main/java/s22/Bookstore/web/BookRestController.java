fc3057de97d52f99372b06079d247c80043cc3ce
leksu70 <82502776+leksu70@users.noreply.github.com>
2022-09-27 16:55:11 +0300

    W39 ti: books/categories: DELETE

--- src/main/java/s22/Bookstore/web/BookRestController.java
+++ src/main/java/s22/Bookstore/web/BookRestController.java
@@ -2,11 +2,18 @@ package s22.Bookstore.web;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,16 +30,33 @@ public class BookRestController {
 	@Autowired
 	private CategoryRepository catrepository;
 
-	// RESTful service to get all books
+	// RESTful service to get all books - toimii
 	@GetMapping(value={"/books", "/books/"})
-	public @ResponseBody Iterable<Book> bookListRest() {	
+	// public @ResponseBody Iterable<Book> bookListRest() {	 ResponseBody:ä ei tarvita, kun on oma RestController käytössä
+	public Iterable<Book> bookListRest() {
         return bookrepository.findAll();
     }
 	
-	@GetMapping(value="/books/{id}")
-	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
-    	return bookrepository.findById(bookId);
-    } 
+	// RESTful service to save a book - Ei toimi
+	@PostMapping(value="/books")
+	public Iterable<Book> addBookRest(Book book, Model model) {
+		System.out.println("REST:Book:" + book.getTitle() + " " + book.getId() + " book:" + book);
+		bookrepository.save(book);
+		return bookrepository.findAll();
+	}
 	
+	// RESTful service to get book by id - toimii
+	@GetMapping(value="/books/{id}")
+	public Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookrepository.findById(bookId);
+    }
+	
+	// RESTful service to delete id book - toimii
+	@DeleteMapping("/books/{id}")
+	public Iterable<Book> deleteBookRest(@PathVariable("id") Long bookId, Model model) {
+		System.out.println("RESTful: bookId:" + bookId);
+		bookrepository.deleteById(bookId);
+		return bookrepository.findAll();
+	}
 	
 }
--- src/main/java/s22/Bookstore/web/CategoryRestController.java
+++ src/main/java/s22/Bookstore/web/CategoryRestController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,17 +25,25 @@ public class CategoryRestController {
 	@Autowired
 	private CategoryRepository catrepository;
 
-	// RESTful service to get all categories
+	// RESTful service to get all categories - toimii
 	@GetMapping(value={"/categories", "/categories/"})
-	public @ResponseBody Iterable<Category> catListRest() {	
+	// public @ResponseBody Iterable<Category> catListRest() {	ResponseBodyä ei tarvita, kun käytössä on RestController.
+	public Iterable<Category> catListRest() {	
         return catrepository.findAll();
     }
 	
-	// RESTful service to get category by id
+	// RESTful service to get category by id - toimii
 	@GetMapping(value="/categories/{id}")
-	public @ResponseBody Optional<Category> findCatRest(@PathVariable("id") Long catId) {	
+	public Optional<Category> findCatRest(@PathVariable("id") Long catId) {	
     	return catrepository.findById(catId);
     } 
 	
+	// RESTful service to delete id category - toimii
+	@DeleteMapping("/categories/{id}")
+	public Iterable<Category> deleteCatRest(@PathVariable("id") Long catId, Model model) {
+		System.out.println("RESTful: catId:" + catId);
+		catrepository.deleteById(catId);
+		return catrepository.findAll();
+	}
 	
 }
